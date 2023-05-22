ef9ff860f9f343d1e180ded28cbabe4337f08873
Pedro Zandoná <pedruco.zandona@gmail.com>
2023-05-22 20:05:29 -0300

    ImplementandoPosition

--- src/BoardLayer/Position.java
+++ src/BoardLayer/Position.java
@@ -2,4 +2,35 @@ package BoardLayer;
 
 public class Position {
 
+	//Coluna e Linha
+	private Integer row;
+	private Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	
+	//Mudar Coluna e Linha
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString(){
+		return row+", "+column;
+	}
+	
+	
 }
